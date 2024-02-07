e7d4293243a362b86aabda7ebfac404b90d67e69
Gabriel Augusto <92814110+GabrielAugustoBer@users.noreply.github.com>
2024-02-07 11:06:31 -0300

    api + css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import CartButton from '../CartButton/CartButton';
 import SearchBar from '../SearchBar/SearchBar';
+
+import './Header.css';
 
 function Header() {
   return (
     <header className="header">
       <div className="container">
-
         <SearchBar />
-        
+        <CartButton />
       </div>
     </header>
   );
